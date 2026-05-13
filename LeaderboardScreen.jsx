ab5a94320b3584a8ce95475b90627c6a76ed0885
henridelamoriniere-ctrl <henridelamoriniere@gmail.com>
2026-05-13 19:09:10 +0200

    Update LeaderboardScreen.jsx

--- LeaderboardScreen.jsx
+++ LeaderboardScreen.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { supabase, getLeaderboard } from '../lib/supabase'
+import { supabase, getLeaderboard } from '../supabase'
 
 const MEDALS = ['🥇', '🥈', '🥉']
 
